6071423514a22b7a44f373af980d7864cec7b6c7
saeedshojaei <saeedshojaei.get@gmail.com>
2020-05-27 17:57:11 +0430

    just number type on input

---
+++ auth.js
@@ -0,0 +1,14 @@
+// template
+(keypress)="numberOnly($event)"
+
+
+
+
+// ts code
+numberOnly(event): boolean {
+	const charCode = (event.which) ? event.which : event.keyCode;
+	if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+	  return false;
+	}
+	return true;
+  }
