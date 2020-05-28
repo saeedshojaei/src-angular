0907384c6205dd3704fd37f098d286a17228c015
saeedshojaei <43775246+saeedshojaei@users.noreply.github.com>
2020-05-28 14:08:02 +0430

    Update index.js
    add function for delete keys when send form to backend

--- index.js
+++ index.js
@@ -4,3 +4,43 @@ console.log('hello2')
 selectItem(e) {
 		this.items[e] = !this.items[e];
 	}
+
+// function for delete keys not select or null
+// get 1 arguments which instanceForm 
+	removeEmpty(obj) {
+		Object.keys(obj).forEach( (key) =>{
+			if (obj[key] && Object.prototype.toString
+				.call(obj[key])
+				.split(' ')[1]
+				.replace(']', '') === 'Object') {
+				this.removeEmpty(obj[key])
+				if (!Object.keys(obj[key]).length) {
+					delete obj[key]
+				}
+			}
+			else if (!obj[key]) delete obj[key]
+		});
+	}
+
+// set as function removeEmpty()
+
+	// for (const item in instanceForm) {
+		// 	if (!instanceForm[item]) {
+		// 		delete instanceForm[item];
+		// 	}
+
+		// 	if (item === 'address') {
+		// 		for (const addressItem in instanceForm[item]) {
+		// 			if (!instanceForm[item][addressItem]) {
+		// 				delete instanceForm[item][addressItem];
+		// 			}
+		// 		}
+		// 	}
+		// }
+
+		// if (!Object.keys(instanceForm.address).length) {
+		// 	delete instanceForm.address;
+		// }
+
+
+
