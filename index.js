a7e0aa0fb0d0f224c77067c73f6f3d94fbaf30fc
saeedshojaei <43775246+saeedshojaei@users.noreply.github.com>
2020-06-02 11:03:09 +0430

    Update index.js
    add function by find() java script for show this moment in other element

--- index.js
+++ index.js
@@ -63,4 +63,36 @@ selectItem(e) {
 		// }
 // }
 
+// checked id when select value and show on other element in html
+// ts
+showPhonePrefix: Object;
+	getProvince(countryId) {
+		if (this.previousCountry !== this.form.value.address.countryId) {
+			this.previousCountry = this.form.value.address.countryId;
+			this.cityService
+				.getProvinceByCountries(countryId)
+				.subscribe((data) => {
+					this.provinceInput = false;
+					this.mobileInput = false;
+					this.provinces = data;
+					this.showPhonePrefix = this.countrys.find(
+						(item) => item.id.toString() === countryId.toString(),
+					);
+				});
+		}
+	}
+// html
+// when select item this element
+<div class="form-group">
+                                    <label for="inputCountry">{{'generic.country' | translate}}</label>
+                                    <nb-select placeholder="{{'generic.countryPlaceholder' | translate}}" fullWidth
+                                        dir="rtl" id="inputCountry" hero
+                                        [status]="(f('address?.countryId')?.invalid || f('address?.countryId')?.errors) ? 'danger' : 'basic'"
+                                        formControlName="countryId" (selectedChange)="getProvince($event)">
+                                        <nb-option *ngFor="let showCountry of countrys; index as i"
+                                            [value]="showCountry?.id">{{showCountry?.countryName}}</nb-option>
+                                    </nb-select>
+                                </div>
 
+// chagne and show in here
+ <span class="pl-3"  class="placeholder-dir phonePrefix">  {{showPhonePrefix?.phonePrefix}}</span> 
